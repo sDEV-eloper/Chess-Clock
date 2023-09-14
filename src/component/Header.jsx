d3f26fc50774f7ed5a1d26aa0d1d7ca06b67ba6b
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-14 21:41:24 +0530

    button click sounde added

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+    const [audio] = useState(new Audio("/click_sound.mp3"));
+
+
+  const start = () => {
+    audio.currentTime=0;
+    audio.play()
+  }
   return (
 <div className='flex justify-center p-8 bg-gray-200'>
 <div className='flex justify-evenly w-full'>
-<button className='text-white bg-blue-600 px-12 py-5 hover:bg-[#8510fb] rounded-lg active:scale-95 shadow-md shadow-black transition duration-150'>Reset</button>
-<button className='text-white bg-blue-600 px-12 py-5 hover:bg-[#8510fb] rounded-lg active:scale-95 shadow-md shadow-black transition duration-150'>Play|Pause</button>
-<button className='text-white bg-blue-600 px-12 py-5 hover:bg-[#8510fb] rounded-lg active:scale-95 shadow-md shadow-black transition duration-150'>Set Time</button>
+<button onClick={()=>start()} className='text-white bg-blue-600 px-12 py-5 hover:bg-[#8510fb] rounded-lg active:scale-95 shadow-md shadow-black transition duration-150'>Reset</button>
+<button onClick={()=>start()} className='text-white bg-blue-600 px-12 py-5 hover:bg-[#8510fb] rounded-lg active:scale-95 shadow-md shadow-black transition duration-150'>Play|Pause</button>
+<button onClick={()=>start()} className='text-white bg-blue-600 px-12 py-5 hover:bg-[#8510fb] rounded-lg active:scale-95 shadow-md shadow-black transition duration-150'>Set Time</button>
 </div>
 </div>
   )
