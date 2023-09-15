62c946ba25806b153a1915f54e9b7aaef0846495
sDEV-eloper <thisissurajdev@gmail.com>
2023-09-15 14:51:36 +0530

    added moves counter

--- src/component/ChessClock.jsx
+++ src/component/ChessClock.jsx
@@ -3,9 +3,12 @@ import Header from "./Header";
 
 
 const ChessClock = () => {
+
   const [activeTimer, setActiveTimer] = useState(1); // 1 for timer1, 2 for timer2
   const [isTimer1Running, setIsTimer1Running] = useState(false);
   const [isTimer2Running, setIsTimer2Running] = useState(false);
+  let [p1MovesCount, setP1MovesCount]=useState(0)
+  let [p2MovesCount, setP2MovesCount]=useState(0)
   
   // Time 1
   const initialTime = 1200; // 20 minutes in seconds
@@ -54,10 +57,12 @@ if (activeTimer === 1) {
       setActiveTimer(2);
       setIsTimer1Running(false);
       setIsTimer2Running(true);
+      setP1MovesCount(p1MovesCount+1)
           } else {
       setActiveTimer(1);
       setIsTimer1Running(true);
       setIsTimer2Running(false);
+      setP2MovesCount(p2MovesCount+1)
       }
   };
   const handleKeyPress = (event) => {
@@ -104,8 +109,8 @@ if (activeTimer === 1) {
           </div>
         </div>
         <div className="bg-gray-300 flex justify-center text-center gap-1">
-          <p className="bg-green-500 w-full p-3 text-2xl font-bold">Moves 1</p>
-          <p className="bg-green-500 w-full p-3 text-2xl font-bold">Moves 2</p>
+          <p className="bg-green-500 w-full p-3 text-2xl font-bold">Moves {p1MovesCount}</p>
+          <p className="bg-green-500 w-full p-3 text-2xl font-bold">Moves {p2MovesCount}</p>
         </div>
       </div>
     </div>
